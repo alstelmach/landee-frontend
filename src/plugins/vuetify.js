59321e9dd22f325bb4e42eb13b5adb733f09fc36
olekstelmach <aleksander.stelmach.ext@talabat.com>
2022-12-03 18:04:50 +0100

    Installed vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,14 @@
+﻿import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetifyOptions = {
+  components,
+  directives,
+};
+
+const vuetify = createVuetify(vuetifyOptions);
+
+export default vuetify;
